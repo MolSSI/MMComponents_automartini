15242d282d59c1373f7b4f3b6fa26171d1492cd1
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-05 01:43:05 -0500

    updated ComputeInput and ParamInput models

--- mmic_param/models/input.py
+++ mmic_param/models/input.py
@@ -3,12 +3,16 @@ from mmelemental.models.forcefield import ForceField
 from mmelemental.models.molecule.mm_molecule import Molecule
 from mmelemental.models.util.input import FileInput
 from pydantic import Field
-from typing import Union
+from typing import Union, Optional
 
 class ParamInput(Base):
-	forcefield: Union[ForceField, str] = Field(..., description = 'Force field object or name e.g. charmm36. See :class:`ForceField`.')
+	forcefield: str = Field(..., description = 'Force field name e.g. charmm36.')
+	solv_forcefield: Optional[str] = Field(None, description = 'Solvent force field name e.g. tip3p.')
 	mol: Molecule = Field(..., description = 'Molecule object. See :class: ``Molecule``.')
+	engine: str = Field(..., description = 'Engine name. See supported engines in :tuple:``mmic_param.components.engines``.')
 
 class ComputeInput(Base):
-	forcefield: Union[FileInput, str] = Field(..., description = 'Force field file object or name e.g. charmm36. See :class:`FileInput`.')
-	mol: FileInput = Field(..., description = 'Molecule file object. See :class:`FileInput`.')
+	forcefield: str = Field(..., description = 'Force field file name e.g. charmm36.')
+	solv_forcefield: Optional[str] = Field(None, description = 'Solvent force field name e.g. tip3p.')
+	mol: str = Field(..., description = 'Molecule file contents.')
+	engine: str = Field(..., description = 'Engine name. See supported engines in :tuple:``mmic_param.components.engines``.')
