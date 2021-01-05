acdbc6a0356ab01591a50d1c5d9bcace602a7063
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-05 01:43:46 -0500

    updated Field types in ComputeOutput model

--- mmic_param/models/output.py
+++ mmic_param/models/output.py
@@ -2,11 +2,12 @@ from mmelemental.models.base import Base
 from mmelemental.models.forcefield import ForceField
 from mmelemental.models.molecule.mm_molecule import Molecule
 from mmelemental.models.util.input import FileInput
+from mmelemental.models.util.output import ComputeOutput
 from pydantic import Field
 
-class ComputeOutput(Base):
-	forcefield: FileInput = Field(..., description = 'Force field params file object. See :class:`FileInput`.')
-	mol: FileInput = Field(..., description = 'Molecule file object. See :class: ``FileInput``.')
+class ComputeOutput(ComputeOutput):
+	forcefield: str = Field(..., description = 'Force field params file string object.')
+	mol: str = Field(..., description = 'Molecule file string object.')
 
 class ParamOutput(Base):
 	forcefield: ForceField = Field(..., description = 'Force field object. See :class:`ForceField`.')
