3a237debcfb5e4b1759e68d506cfdcc2c8ca83fd
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-01-05 21:12:43 -0500

    added PostComponent

--- mmic_param/tests/test_gromacs.py
+++ mmic_param/tests/test_gromacs.py
@@ -12,20 +12,31 @@ def test_automartini_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmic_param" in sys.modules
 
-
-from mmic_param.components import PrepComponent, ComputeComponent
+from mmic_param.components import PrepComponent
 from mmic_param.models.input import ParamInput, ComputeInput
+from mmic_param.components.gmx.compute_component import ComputeComponent 
+from mmic_param.components.gmx.post_component import PostComponent
 
-fpath = os.path.join('mmic_param', 'data', 'ibuprofen', 'ibu.gro')
+fpath = os.path.join('mmic_param', 'data', 'molecules', 'dialanine.pdb')
 
 mol = mmelemental.models.molecule.mm_molecule.Molecule.from_file(fpath)
 
-# Prepare input
-paramInput = ParamInput(mol=mol, forcefield='charmm27')
+############## VACUUM ################
+# Prepare input for molecule in vacuum
+paramInput = ParamInput(mol=mol, forcefield='amber99', engine='gmx')
 computeInput = PrepComponent.compute(paramInput)
 
-# Compute FF params for molecule
+# Compute FF params for molecule using gromacs
 computeOutput = ComputeComponent.compute(computeInput)
 
 # Convert file objects to MM objects
-# paramOutput = PostComponent.compute(computeOutput)
+paramOutput = PostComponent.compute(computeOutput)
+print(paramOutput.mol)
+
+############## SOLVENT ##############
+# Prepare input for molecule in vacuum
+# paramInput = ParamInput(mol=mol, forcefield='amber99', solv_forcefield='tip3p', engine='gmx')
+# computeInput = PrepComponent.compute(paramInput)
+
+# Compute FF params for molecule using gromacs
+# computeOutput = ComputeComponent.compute(computeInput)
