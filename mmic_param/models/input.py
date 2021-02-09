3c76bf2b94cb04b9f21dd59eab83c413e82bab30
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-09 02:46:06 -0500

    removed unused imports

--- mmic_param/models/input.py
+++ mmic_param/models/input.py
@@ -1,18 +1,28 @@
 from mmelemental.models.base import Base
-from mmelemental.models.forcefield import ForceField
 from mmelemental.models.molecule import Mol
-from mmelemental.models.util.input import FileInput
 from pydantic import Field
-from typing import Union, Optional
+from typing import Optional
+
 
 class ParamInput(Base):
-	forcefield: str = Field(..., description = 'Force field name e.g. charmm36.')
-	solv_forcefield: Optional[str] = Field(None, description = 'Solvent force field name e.g. tip3p.')
-	mol: Mol = Field(..., description = 'Molecule object. See :class: ``Molecule``.')
-	engine: str = Field(..., description = 'Engine name. See supported engines in :tuple:``mmic_param.components.engines``.')
+    forcefield: str = Field(..., description="Force field name e.g. charmm36.")
+    solv_forcefield: Optional[str] = Field(
+        None, description="Solvent force field name e.g. tip3p."
+    )
+    mol: Mol = Field(..., description="Molecule object. See :class: ``Molecule``.")
+    engine: str = Field(
+        ...,
+        description="Engine name. See supported engines in :tuple:``mmic_param.components.engines``.",
+    )
+
 
 class ComputeInput(Base):
-	forcefield: str = Field(..., description = 'Force field file name e.g. charmm36.')
-	solv_forcefield: Optional[str] = Field(None, description = 'Solvent force field name e.g. tip3p.')
-	mol: str = Field(..., description = 'Molecule file contents.')
-	engine: str = Field(..., description = 'Engine name. See supported engines in :tuple:``mmic_param.components.engines``.')
+    forcefield: str = Field(..., description="Force field file name e.g. charmm36.")
+    solv_forcefield: Optional[str] = Field(
+        None, description="Solvent force field name e.g. tip3p."
+    )
+    mol: str = Field(..., description="Molecule file contents.")
+    engine: str = Field(
+        ...,
+        description="Engine name. See supported engines in :tuple:``mmic_param.components.engines``.",
+    )
